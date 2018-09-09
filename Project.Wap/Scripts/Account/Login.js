271b9d5290a03929a671a8d75078a60dbfce690b
awpli <a6818162@163.com>
2018-09-09 10:06:33 +0800

    77

--- Project.Wap/Scripts/Account/Login.js
+++ Project.Wap/Scripts/Account/Login.js
@@ -7,26 +7,20 @@ var pro = pro || {};
 
             $("#btn_Login").click(
                 function () {
-                    var postData = { AccountName: $("#AccountName").val(), Password: $("#Password").val() };
+                    var postData = { LoginName: $("#AccountName").val(), Password: $("#Password").val() };
 
                     $.ajax({
                         dataType: 'json',
                         type: 'POST',
                         contentType: 'application/json',
-                        url: "/Account/Login",
+                        url: pro.globalConfig.apiUrl + "/api/Author/Login",
                         data: JSON.stringify(postData),
                         cache: false,
                         async: false,
                         success: function (data) {
-                            //alert(JSON.stringify(data));
+                            alert(JSON.stringify(data));
 
-                            var returnUrl = $("#returnUrl").val();
-                            if (data.success) {
-                                
-                                window.location.href = (returnUrl != "" ? returnUrl : "/Home/Index");
-                            } else {
-                                $("#LoginErrorPlace").css("display","");
-                            }
+                           
 
                         }
                     });
--- Project.Wap/Scripts/Config/globalConfig.js
+++ Project.Wap/Scripts/Config/globalConfig.js
@@ -6,6 +6,8 @@
           "AddSuccess": "操作成功！",
           "UpdateSuccess": "操作成功！",
           "DeleteSuccess": "操作成功！"
-      }
+      },
+      apiUrl: "http://localhost:8133/"
+
     };
 })();
